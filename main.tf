69236a3042133cfe44dc25b9182afb1197397a65
Sujay Samanta <sujay.samanta@hashicorp.com>
2026-01-14 15:50:58 +0000

    Add initial Terraform configuration for AWS S3 bucket management

---
+++ main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_version = ">= 1.0"
+  cloud { 
+    
+    organization = "SujaysTerraformLab" 
+
+    workspaces { 
+      name = "automatic-fiesta" 
+    } 
+  } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
+  }
+}
+
+# Configure the Doormat Provider
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+
+  role_arn = "arn:aws:iam::886363944443:role/sample_dev-custom_role"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "main" {
+  bucket = var.bucket_name
+
+  tags = merge(
+    var.tags,
+    {
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  )
+}
+
+# Configure S3 bucket versioning
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Configure S3 bucket server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
